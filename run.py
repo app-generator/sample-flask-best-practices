7774843112559a3880fcf48bda38eeccc407c7eb
RS Official <ravi01prasad@gmail.com>
2022-05-09 01:09:35 +0530

    auto port pickup

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app, db
 
 app = create_app(config_name='development')
@@ -5,4 +6,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = os.environ.get("PORT",5000)
+    app.run(host='0.0.0.0', port=port, debug=False)
